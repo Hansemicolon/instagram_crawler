58e96ec966ed3886e4dd0c5b3596d70a30c5d2d4
박종후 <parkjongwho@jongwho.local>
2022-01-19 22:58:48 +0900

    insta parser edit

--- instaParser.py
+++ instaParser.py
@@ -1,8 +1,11 @@
+import pandas as pd
 import pickle
 import json
 
 
 def str_cleaner(text):
+    if not isinstance(text, str):
+        return ""
     text = text.replace('\n', '').replace('\r', '').replace('\t', '')
     return text
 
@@ -22,11 +25,15 @@ def parse_section_media(media_list):
             sect = media.get('media')
             short_code = sect.get('code')
             sect_media = sect.get('caption')
-            published_at = sect_media.get('created_at')
-            media_id = sect_media.get('media_id')
-            content = sect_media.get('text')
-            tag_list = list(map(parse_tag, [x for x in content.split("#")
-                                            if content[content.find(x)-1] == '#' and content.find(x) != 0]))
+
+            published_at = sect_media.get('created_at') if sect_media is not None else sect.get('taken_at')
+            media_id = sect_media.get('media_id') if sect_media is not None else sect.get('pk')
+            content = sect_media.get('text', "") if sect_media is not None else sect.get('caption', "")
+            if content is None:
+                tag_list = []
+            else:
+                tag_list = list(map(parse_tag, [x for x in content.split("#")
+                                                if content[content.find(x)-1] == '#' and content.find(x) != 0]))
             item = {'short_code': short_code, 'media_id': media_id, 'content': str_cleaner(content),
                     'published_at': published_at, 'tag_list': tag_list}
 
@@ -47,11 +54,19 @@ def parse_main(file_name):
             media_list = body.get('sections')
 
         result.extend(parse_section_media(media_list))
-    return result
+    df = pd.DataFrame.from_records(result)
+    return df
 
 
 if __name__ == '__main__':
+    result_df = pd.DataFrame()
     import pprint
-    pprint.pprint(parse_main('estj_res'))
+
+    for t in ['estj', 'enfp', 'infp', 'istj', 'mbti', 'esfp']:
+        result_df = result_df.append(parse_main(f"{t}_res"))
+    result_df = result_df.drop_duplicates(['short_code'], keep='first')
+
+    with open('mbti_result.pickle', 'wb') as f:
+        pickle.dump(result_df, f, pickle.HIGHEST_PROTOCOL)
 
 
